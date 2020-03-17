7c5a1add230ec5ac68770503302b301e3d01f08c
Dimitris Papaioannou <dpapaioannou@semitron.gr>
2020-03-17 19:41:10 +0200

    Initial commit

---
+++ TheMoviesDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMoviesDB
+//
+//  Created by Dimitris Papaioannou on 17/3/20.
+//  Copyright © 2020 papadim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+

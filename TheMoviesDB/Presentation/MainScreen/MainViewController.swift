74c25ae8c2a18cd82586707429cd7c4c51f0abf5
Dimitris Papaioannou <dpapaioannou@semitron.gr>
2020-03-17 19:52:29 +0200

    .gitignore added

--- TheMoviesDB/Presentation/MainScreen/MainViewController.swift
+++ TheMoviesDB/Presentation/MainScreen/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  TheMoviesDB
 //
 //  Created by Dimitris Papaioannou on 17/3/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
